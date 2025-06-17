8cc0b317f7db9dca5dbadeeefb3d616e42556327
Ameen <Saddiq.Ameen@ingrammicro.com>
2025-06-17 12:14:33 +0400

    Fix port binding for Render

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 # Triggering redeploy after adding import os
 
+import os  # Make sure this is at the top of the file!
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))  # Render dynamically assigns this
+    socketio.run(app, host='0.0.0.0', port=port)
+
 from flask import Flask, render_template
 from flask_socketio import SocketIO
 import threading
