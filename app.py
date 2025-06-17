8dc7f749fc7d50972334868f9698be1873d016fb
Ameen <Saddiq.Ameen@ingrammicro.com>
2025-06-17 11:56:37 +0400

    Trigger redeploy: added comment

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
+# Triggering redeploy after adding import os
+
 from flask import Flask, render_template
 from flask_socketio import SocketIO
 import threading
 import time
 
+import os
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")
 
@@ -24,4 +27,8 @@ def index():
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))  # Use Render's assigned port or default to 5000
     socketio.start_background_task(fetch_threat_data)  # Replaces threading.Thread
+    socketio.run(app, host='0.0.0.0', port=port)
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))  # Use Render's assigned port or default to 5000
     socketio.run(app, host='0.0.0.0', port=port)
